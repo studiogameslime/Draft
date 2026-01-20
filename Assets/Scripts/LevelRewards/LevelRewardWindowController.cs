a5c9c94deb949ea366516ca2de16423a084cbacb
studiogameslime <studiogameslime@gmail.com>
2026-01-20 22:51:24 +0200

    scroll to middle when open the window

--- Assets/Scripts/LevelRewards/LevelRewardWindowController.cs
+++ Assets/Scripts/LevelRewards/LevelRewardWindowController.cs
@@ -337,6 +337,8 @@ public class LevelRewardWindowController : MonoBehaviour
         root.SetActive(true);
 
         popupAnimator.OpenFromRect(buttonRect);
+        StartCoroutine(ScrollToCurrentLevelNextFrame());
+
     }
 
 
